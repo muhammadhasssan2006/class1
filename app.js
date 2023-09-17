22c381f995c18598954aaec879d4f3941311c69e
hassan <hassanweb365@gmail.com>
2023-09-17 15:25:51 +0500

    6 chapter complete

---
+++ app.js
@@ -0,0 +1,46 @@
+// alert("Hello world");
+// alert("Kia hal ha janab!");
+
+// variables for strings
+
+// var name = "Hassan   ";
+// var country = "pakistan";
+
+// alert(a)
+// console.log(name + country)
+
+// variable for numbers
+
+// var a = 1;
+// var b = 3;
+// var c = 5;
+
+// var d = b + c;
+
+// console.log(d)
+
+// screenRedOrBlue screen_red_or_blue
+
+// num++ post increment
+// num--  post decrement
+
+// var num = 3;
+// num++
+
+// var num2 = 4
+
+// num2--
+
+// var sum = num * num2
+
+// console.log(sum)
+// console.log(num)
+
+
+// var num = 3
+// var num2 = 4 * num--
+
+// console.log(num2)
+// console.log(num)
+
+
